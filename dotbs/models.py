134cf094906560ebd79b1e3f9f9883b09e6016a6
Konstantin Itskov <konstantin.itskov@kovits.com>
2014-10-05 03:16:00 -0400

    some more updates

--- dotbs/models.py
+++ dotbs/models.py
@@ -2,9 +2,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 import pandas as pd
+from selenium import webdriver
 
 class Model():
     def __init__(self):
+	self.driver = webdriver.PhantomJS()
         self._labels = {
             0 : 'Reliable',
             1 : 'Not Reliable'
